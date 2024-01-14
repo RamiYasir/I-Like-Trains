6d8f55a1c86faba68d2372fc9e8cd7413e1a54af
Rami Yasir <ramimilkyasir@gmail.com>
2024-01-14 14:17:40 +0000

    create jupyter notebook and organise supplier by expenses paid to them

--- utilities.py
+++ utilities.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import re
+from collections import Counter, defaultdict
 
-df = pd.read_csv('resources/sanitised-data.csv')
-df.reset_index()
-
-def total_expenses() -> float:
+def total_expenses(df: pd.DataFrame) -> float:
     total_spend = 0
     for expense in df['Net Amount']:
         float_expense = float(re.sub(",", "", expense))
@@ -12,8 +10,8 @@ def total_expenses() -> float:
 
     return total_spend
 
-
-def get_all_distinct_entries(column: str) -> list:
+ 
+def get_all_distinct_entries(df: pd.DataFrame, column: str) -> list:
     distinct_entries = []
     for entry in df[column]:
         if entry in distinct_entries:
@@ -22,10 +20,9 @@ def get_all_distinct_entries(column: str) -> list:
     return distinct_entries
 
 
-
-def sort_suppliers_by_service_area() -> dict:
-    service_to_supplier = {service: [] for service in get_all_distinct_entries("Service Area")}
-    for supplier in get_all_distinct_entries("Supplier Name"):
+def sort_suppliers_by_service_area(df: pd.DataFrame) -> dict:
+    service_to_supplier = {service: [] for service in get_all_distinct_entries(df, "Service Area")}
+    for supplier in get_all_distinct_entries(df, "Supplier Name"):
         row = df.loc[df["Supplier Name"] == supplier].iloc[0]
         service_value = row["Service Area"]
         supplier_value = row["Supplier Name"]
@@ -35,8 +32,10 @@ def sort_suppliers_by_service_area() -> dict:
         service_to_supplier[service_value].append(supplier_value)
 
     return service_to_supplier
-            
 
-nice_dictionary = sort_suppliers_by_service_area()
-print(f"${nice_dictionary.popitem()} \n\n\n") 
-print(nice_dictionary.popitem())
+
+def total_expenses_per_supplier(df: pd.DataFrame, supplier: str):
+    results = df.loc[df["Supplier Name"] == supplier]
+    total_expense = total_expenses(results)
+
+    return total_expense
